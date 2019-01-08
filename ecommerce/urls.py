8f53eb47f3bbdfc6ec29632e01d927d3efdd0a43
sriaknt132 <srikantcs132@gmail.com>
2019-01-08 13:48:07 +0530

    here we adding the url path for the contact and about page importing pages

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from . views import home_page       #this need to import after creating views
+from . views import home_page,about_page,contact_page      #this need to import after creating views
 
 urlpatterns = [
     url(r'^$', home_page),
+    url(r'^about/$', about_page),
+    url(r'^contact/$', contact_page),
     url(r'^admin/', admin.site.urls),
 
 ]
