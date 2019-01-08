b732124fed65bfe91e752b552983b85fe2f7c9c9
sriaknt132 <srikantcs132@gmail.com>
2019-01-08 14:06:42 +0530

    here we are adding some more context as content

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -4,18 +4,21 @@ from django.shortcuts import render
 
 def home_page(request):
     context ={
-        "title": "Hello World!"
+        "title": "Hello World!",
+        "content": "Welcome to Homepage."
     }
     return render(request,"home_page.html",context) #we can also rener html directly
 
 def about_page(request):
     context ={
-        "title": "aboutpage"
+        "title": "aboutpage",
+        "content":"Welcome to Aboutpage "
     }
     return render(request,"home_page.html",context) #{} here we pass context here i.e additional information
 
 def contact_page(request):
     context ={
-        "title": "contact_page"
+        "title": "contact_page",
+        "content":"Welcome to contactpage "
     }
     return render(request,"home_page.html",context)
