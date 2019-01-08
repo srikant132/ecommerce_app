b92f2f1fd7b4eb1e2df4f50bc41fca7f1578c85f
sriaknt132 <srikantcs132@gmail.com>
2019-01-08 13:46:55 +0530

    here we add context to change some of the data

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,4 +3,19 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request,"home_page.html",{}) #we can also rener html directly
+    context ={
+        "title": "Hello World!"
+    }
+    return render(request,"home_page.html",context) #we can also rener html directly
+
+def about_page(request):
+    context ={
+        "title": "aboutpage"
+    }
+    return render(request,"home_page.html",context) #{} here we pass context here i.e additional information
+
+def contact_page(request):
+    context ={
+        "title": "contact_page"
+    }
+    return render(request,"home_page.html",context)
