a7b0bc6702b91c137d368705641c82581f57c498
sriaknt132 <srikantcs132@gmail.com>
2019-01-09 19:43:19 +0530

    creating url list for the classbased view and function based view

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -19,6 +19,8 @@ from django.conf.urls.static import static
 from django.conf.urls import url
 from django.contrib import admin
 
+from products.views import ProductListView, product_list_view
+
 from . views import home_page,about_page,contact_page,login_page,register_page     #this need to import after creating views
 
 urlpatterns = [
@@ -27,6 +29,8 @@ urlpatterns = [
     url(r'^contact/$', contact_page),
     url(r'^login/$', login_page),
     url(r'^register/$', register_page),
+    url(r'^products/$', ProductListView.as_view()),
+    url(r'^products_fbv/$', product_list_view),
     url(r'^admin/', admin.site.urls),
 
 ]
