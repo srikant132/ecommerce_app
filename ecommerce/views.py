2dfffe4f9a24db2f842d32fdc5d3322b4274cc82
sriaknt132 <srikantcs132@gmail.com>
2019-01-09 11:07:29 +0530

    importing Registration form from forms.py

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,15 +1,18 @@
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 
 
-from .forms import ContactForm,LoginForm          #here  we are importing from forms.py Default django form
+from .forms import ContactForm,LoginForm,RegisterForm         #here  we are importing from forms.py Default django form
 
 def home_page(request):
     context ={
         "title": "Hello World!",
-        "content": "Welcome to Homepage."
+        "content": "Welcome to Homepage.",
+
     }
+    if request.user.is_authenticated():
+        context["premium_content"] =  "this is premium content"
     return render(request,"home_page.html",context) #we can also rener html directly
 
 def about_page(request):
@@ -54,7 +57,7 @@ def login_page(request):
             login(request,user)
             #redirect to a success page
             #context['form'] = LoginForm()
-            return redirect("/login")
+            return redirect("/")
         else:
             print("Error")
 
@@ -62,9 +65,17 @@ def login_page(request):
     return render(request,"auth/login.html", context)
 
 
-
+User = get_user_model()
 def register_page(request):
-    form = LoginForm(request.POST or None)
+    form = RegisterForm(request.POST or None)
+    context = {
+            "form":form
+        }
     if form.is_valid():
-        print(forms.cleaned_data)
-    return render(request,"auth/register.html",{})
+        print(form.cleaned_data)
+        username =form.cleaned_data.get("username")
+        email =form.cleaned_data.get("email")
+        password =form.cleaned_data.get("password")
+        new_user = User.objects.create_user(username,email,password)
+        print(new_user)
+    return render(request,"auth/register.html",context)
