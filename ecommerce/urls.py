33537da10bfe61125909cb4499e800407a5947b3
sriaknt132 <srikantcs132@gmail.com>
2019-01-09 11:06:09 +0530

    hee added path for register_page and imported from views.

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from . views import home_page,about_page,contact_page,login_page     #this need to import after creating views
+from . views import home_page,about_page,contact_page,login_page,register_page     #this need to import after creating views
 
 urlpatterns = [
     url(r'^$', home_page),
     url(r'^about/$', about_page),
     url(r'^contact/$', contact_page),
     url(r'^login/$', login_page),
+    url(r'^register/$', register_page),
     url(r'^admin/', admin.site.urls),
 
 ]
