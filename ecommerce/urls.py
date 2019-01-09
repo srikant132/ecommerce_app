f6ac5458d8be6ff74e97f6923c0a0a341ae7d30e
sriaknt132 <srikantcs132@gmail.com>
2019-01-09 12:44:08 +0530

    hee we importing in urls.py for accessing the sdstatic files.

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.conf.urls import url
 from django.contrib import admin
 
@@ -27,3 +30,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
 ]
+
+if  settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
