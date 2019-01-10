8ddc9d5e3b55fad7fb167b1d434511f657d796bc
sriaknt132 <srikantcs132@gmail.com>
2019-01-10 16:47:14 +0530

    here we get error due to generic.list because here we want import DetailView.

--- products/views.py
+++ products/views.py
@@ -1,8 +1,8 @@
 # from django.views import ListView                    #This is error we can not import in this way
-# from django.views.generic import ListView            #we can also import in this way
-from django.views.generic.list import ListView         #impoting ListView from django views
-from django.shortcuts import render
-
+# from django.views.generic import ListView            #we can also import in this
+#impoting ListView from django views
+from django.views.generic import ListView,DetailView
+from django.shortcuts import render,get_object_or_404
 
 from .models import Product           #importing model from models
 # Create your views here.
@@ -25,3 +25,25 @@ def product_list_view(request):
     }
 
     return render (request,"products/list.html",context)
+
+
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+ #every single classBased view has his method it gets the context for any given queryset or view
+    def get_context_data(self,*args,**kwargs):
+        context = super(ProductDetailView,self).get_context_data(*args,**kwargs)
+        print(context)
+        # context['abc'] =123
+        return context
+
+
+#function Based View
+def product_detail_view(request,pk=None,*args,**kwargs):
+    # instance = Product.objects.get(pk=pk)  #id
+    instance = get_object_or_404(Product,pk=pk)
+    context = {
+        'object' : instance
+    }
+    return render (request,"products/detail.html",context)
