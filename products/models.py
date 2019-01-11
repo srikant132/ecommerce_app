c051eda4758946cdc6452dc9c5a941d559906988
sriaknt132 <srikantcs132@gmail.com>
2019-01-11 13:18:29 +0530

    it gave more robust filename.

--- products/models.py
+++ products/models.py
@@ -19,18 +19,52 @@ def upload_image_path(instace,filename):
     name,ext = get_filename_ext(filename)
     final_filename = f'{ new_filename}{ext}'.format(new_filename=new_filename,ext=ext)
     return "products/{new_filename}/{final_filename}".format(
-            new_filename=new_filename,
-            final_filename=final_filename               #it gives us more robust way of the  file name
+            new_filename=new_filename,                        #it gives us more robust way of the  file name
+            final_filename=final_filename
         )
+
+#here  w creatin a function for ProductManager
+
+class ProductQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+    def featured(self):
+        return self.filter(featured=True, active=True)
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def all(self):
+        return self.get_queryset().active()
+
+    def featured(self):          #Product.objects.featured()
+        return self.get_queryset().featured()
+
+    def get_by_id(self,id):
+        qs = self.get_queryset().filter(id=id)                                #this portion takes Product.objects ==  self.get_queryset()
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
 
 
 class Product(models.Model):
     title       = models.CharField(max_length=120)
+    slug        = models.SlugField(blank=True)
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2,max_digits=20,default=39.99)
     image       = models.ImageField(upload_to='upload_image_path',null=True, blank=True)
- #it's a function that rep of model it's instance field
- #it create the product instace different name with their title
+    featured    = models.BooleanField(default=False)
+    active      = models.BooleanField(default=True)
+
+
+
+    objects = ProductManager()
+                                                                              #it's a function that rep of model it's instance field
+                                                                              #it create the product instace different name with their title
+
     def __str__(self):
         return self.title
 
