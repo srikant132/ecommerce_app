39e74e9582617ae8b57dd4ac5ca80b762ffee141
sriaknt132 <srikantcs132@gmail.com>
2019-01-17 18:41:57 +0530

    rendering some product

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from products.models import Product
 from .models import Cart
 
 # Create your views here.
@@ -12,3 +14,16 @@ def cart_home(request):
     cart_obj.total =total
     cart_obj.save()
     return render(request,"carts/home.html",{})
+
+#here we creating views for the cart
+def cart_update(request):
+    product_id =1
+    product_obj = Product.objects.get(id=product_id)
+    cart_obj,new_obj = Cart.objects.new_or_get(request)
+    if product_obj in cart_obj.products.all():
+        cart_obj.products.remove(products_obj)
+
+    else:
+        cart_obj.products.add(product_obj)  #obj = Product.objects.get(id=1)
+    # return redirect(product_obj.get_absolute_url())
+    return redirect("cart:home")
